4eedfe592efd16bd20a5559d1e9c2cb77dc2d934
Alonso <alonso.jimenez@hotmail.com>
2020-07-29 11:00:21 -0600

    Cambios en el template de calculadora

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,13 +1,14 @@
 #!flask/bin/python
 import json
 from flask import Flask, Response
+from flask import render_template
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return render_template("calculator.html")
 
 @application.route('/', methods=['POST'])
 def post():
